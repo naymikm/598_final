63dd716db635bb101cab7428a65cdcc76e862cc8
Marcus Naymik <marcusanaymik@gmail.com>
2016-04-28 19:13:04 -0700

    finished p2

--- impress/ebolaSim.sh
+++ impress/ebolaSim.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 #genome prep
+##Turn fasta sequence into 1 line of text, \n needed as EOF character for R loading
 #sed 's/N//g' ebola_zaire.fasta | grep -v '^$\|^>' | tr -d '\n' | tr [a-z] [A-Z] > ez.txt
 #echo -e '\n' >> ez.txt
 
 #echo -e "WARNING! If running more than 1 simulation, you must delete the file mutant_reads.fa between runs\n"
 yrs=$1
-
+echo -e "Mutating..."
 Rscript mutate.R "$yrs"
 sed -i -e s/CHROM/#CHROM/ ez.vcf
+echo -e "Annotating..."
 java -jar /home/marcus/bio/TOOLS/snpEff/snpEff.jar ebola_zaire ez.vcf > ez.ann.vcf
 echo -e "\nSee ez.ann.vcf for the annotated vcf file!\n"
 echo -e "Opening HTML Summary..."
--- part_2/pipeline.sh
+++ part_2/pipeline.sh
@@ -4,10 +4,10 @@ usage="\nUsage: bash pipeline.sh [options]\n
 \t-f Samples unpaired fastq file
 \t-g Path to the GATK jar file\n"
 
-while getopts 'ha:b:c:d:g:' flag;
+while getopts 'hg:f:' flag;
 do
  case "${flag}" in
-  a) fq="${OPTARG}" ;;
+  f) fq="${OPTARG}" ;;
   g) gatk="${OPTARG}" ;;
   h) echo -e "$usage"; exit 1;;
   *)
@@ -23,32 +23,39 @@ if [[ -z "$fq" ]] || [[ -z "$gatk" ]]; then
  exit 1
 fi
 
+sample="${fq:10:3}"
 
+echo "$sample"
+
+#samtools 0.1.19
+echo -e "\nRunning sequence alignment...\n"
 bowtie2 -x ./seq_files/ref -U "$fq" -S a_temp_.sam \
 --rg-id A --rg SM:sampleA --rg PL:Illumina --rg LB:libA
 
 samtools view a_temp_.sam -bS -o data_temp_.bam
 
-picard-tools MarkDuplicates INPUT=data_temp_.bam OUTPUT=dedup_temp_.bam METRICS_FILE=metrics_temp_.txt
+samtools sort data_temp_.bam sorted_temp_
+
+picard-tools MarkDuplicates INPUT=sorted_temp_.bam OUTPUT=dedup_temp_.bam METRICS_FILE=metrics_temp_.txt
 
 picard-tools BuildBamIndex INPUT=dedup_temp_.bam
 
-java -jar "$gatk" -T RealignerTargetCreator -R ./seq_files/reference.fasta -I dedup_temp_.bam \
+java -Xmx3g -jar "$gatk" -T RealignerTargetCreator -R seq_files/reference.fasta -I dedup_temp_.bam \
 -o rtc_temp_.intervals
 
-java -jar "$gatk" -T IndelRealigner -R ./seq_files/reference.fasta -I dedup_temp_.bam \
+java -Xmx3g -jar "$gatk" -T IndelRealigner -R seq_files/reference.fasta -I dedup_temp_.bam \
 -targetIntervals rtc_temp_.intervals -o realigned_temp_.bam
 
-java -jar "$gatk" -T UnifiedGenotyper -I realigned_temp_.bam -R ./seq_files/reference.fasta \
+java -Xmx3g -jar "$gatk" -T UnifiedGenotyper -I realigned_temp_.bam -R seq_files/reference.fasta \
 -stand_call_conf 50 -stand_emit_conf 50 -ploidy 2 -glm BOTH -o first_temp_.vcf
 
-java -jar "$gatk" -T BaseRecalibrator -I realigned_temp_.bam -R ./seq_files/reference.fasta \
+java -Xmx3g -jar "$gatk" -T BaseRecalibrator -I realigned_temp_.bam -R seq_files/reference.fasta \
 --knownSites first_temp_.vcf -o recal_temp_.table
 
-java -jar "$gatk" -T PrintReads -I realigned_temp_.bam -R ./seq_files/reference.fasta \
--BQSR recal_temp_.table -EOQ -o FINAL_recal.bam
+java -Xmx3g -jar "$gatk" -T PrintReads -I realigned_temp_.bam -R seq_files/reference.fasta \
+-BQSR recal_temp_.table -EOQ -o "$sample"_recal.bam
 
-java -jar "$gatk" -T UnifiedGenotyper -I FINAL_recal.bam -R ./seq_files/reference.fasta \
--ploidy 2 -glm BOTH -o FINAL_variant_calls.vcf
+java -Xmx3g -jar "$gatk" -T UnifiedGenotyper -I "$sample"_recal.bam -R seq_files/reference.fasta \
+-ploidy 2 -glm BOTH -o "$sample".vcf
 
-#rm *_temp_*
+rm *_temp_*
